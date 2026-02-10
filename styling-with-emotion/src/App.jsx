1aa4c399981206382f14878dc10ab8dc6dbcb8cf
Rob Hess <hessro@oregonstate.edu>
2026-02-10 15:08:09 -0800

    Add code from 2/10 lecture.

--- styling-with-emotion/src/App.jsx
+++ styling-with-emotion/src/App.jsx
@@ -1,16 +1,56 @@
 import { useState } from 'react'
-import { css } from '@emotion/react'
+import { css, Global } from '@emotion/react'
+
+const textColor = "olivedrab"
+
+const globalStyles = css`
+    @import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap');
+    html {
+        font-family: "Roboto", sans-serif;
+    }
+    body {
+        margin: 0;
+    }
+`
 
 function Button(props) {
-    const { children, ...rest } = props
-    return <button {...rest}>{children}</button>
+    const { children, secondary, ...rest } = props
+
+    const styles = css`
+        margin: 8px;
+        color: ${secondary ? "royalblue" : "snow"};
+        background-color: ${secondary ? "whitesmoke" : "royalblue"};
+        border: 2px solid royalblue;
+        padding: 10px;
+        cursor: pointer;
+
+        &:hover {
+            background-color: dodgerblue;
+        }
+    `
+    return <button css={styles} {...rest}>{children}</button>
 }
 
 function PhotoCard(props) {
     const { url, caption, author } = props
     const [ isFaved, setIsFaved ] = useState(false)
+
+    const styles = css`
+        display: flex;
+        gap: 8px;
+        width: 256px;
+        box-shadow: 5px 5px 5px rgba(0, 0, 0, 0.25);
+        border-radius: 8px;
+        img {
+            width: 64px;
+        }
+        button {
+            color: lightpink;
+        }
+    `
+
     return (
-        <div>
+        <div css={styles}>
             <img src={url} alt={caption} />
             <div>
                 <p>{caption}</p>
@@ -26,14 +66,26 @@ function PhotoCard(props) {
 export default function App() {
     const styles = css`
         background-color: lavenderblush;
-        color: mediumvioletred;
+        color: ${textColor};
         margin: 0;
         padding: 12px;
+
+        @media (max-width: 640px) {
+            font-size: 24px;
+        }
     `
 
     return (
         <>
+            <Global styles={globalStyles} />
             <h1 css={styles}>What a boring app...</h1>
+            <Button>My button</Button>
+            <Button secondary>My second button</Button>
+
+            <PhotoCard
+                url="https://picsum.photos/256"
+                caption="My caption"
+                author="Author" />
         </>
     )
 }
